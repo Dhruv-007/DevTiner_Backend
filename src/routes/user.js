58a4fcb02b0a7fef934bc8b58fb690d16a4dba23
Dhruv <joharidhruv19@gmail.com>
2025-05-21 22:01:18 +0530

    Feed api, pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,9 +2,11 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const ConnectionRequestModel = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const userRouter = express.Router();
 
+const SAVE_USER_DATA =  "firstName lastname skills age gender photoUrl"
 // get pending connection api
 
 userRouter.get("/user/request/recieved", userAuth, async(req, res)=>{
@@ -38,7 +40,7 @@ userRouter.get("/user/request/recieved", userAuth, async(req, res)=>{
         {fromUserId:loggedInUser._id, status:"accepted"}
        ]
      }).populate("fromUserId", "firstName lastname skills age gender photoUrl").
-     populate("toUserId", "firstName lastname skills age gender photoUrl")
+     populate("toUserId", SAVE_USER_DATA)
 
      const data = connectionRequest.map((row) => {
       if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
@@ -65,17 +67,39 @@ userRouter.get("/user/request/recieved", userAuth, async(req, res)=>{
 // feed api 
 userRouter.get("/user/feed", userAuth, async (req,res)=>{
     const loggedInUser =  req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit > 50 ? 50 : limit;
+    const skip = (page-1)*limit;
 
     //  1st -- ConnectRequest unique user  Id ( from and to)
     //  2nd --  fetch only ids, now search it on user model , for self user id, 
     //  3r === all ids should not be equal to connectionRequestIds.
    
     // Find all the connection req [send or recieved ]
-    
-    // const connectionRequest = await ConnectionRequest.find({
-    //     $or:[{ fromUserId: loggedInUser._id },
-    //         {toUserId : loggedInUser._id}]
-    // })
+    const connectionRequest = await ConnectionRequest.find({
+        $or:[{ fromUserId: loggedInUser._id },
+            {toUserId : loggedInUser._id}]
+    }).select("fromUserId toUserId")
+
+    // blocked user, tht shouldnt be seend on feed
+
+    const hideUserFromFeed = new Set();
+    connectionRequest.forEach(req => {
+        hideUserFromFeed.add(req.fromUserId.toString())
+        hideUserFromFeed.add(req.toUserId.toString())
+    })
+
+    //  now from db of user , collect all id tht are not present in hidefromFeed and show
+
+    const users = await User.find({
+        $and: [ {_id: { $nin: Array.from(hideUserFromFeed)}},   // $nin means not in this array
+              {_id:{$ne: loggedInUser._id}}   // $ne means not equal to
+        ]}).select(SAVE_USER_DATA).skip(skip).limit(limit)
+
+    res.json({ message:"Date fetched Successfull !!!",
+          users})
+
 })
 
 module.exports = userRouter;
